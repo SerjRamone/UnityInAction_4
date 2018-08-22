b05bea1d7f0a1e68149f1d7c3c79e0e86921f19e
SerjRamone <sergey.greznov@gmail.com>
2018-08-23 00:11:55 +0300

    проверка корректности ориентации персонажа относительно взаимодествуемого объекта

--- Assets/Scripts/DeviceOperator.cs
+++ Assets/Scripts/DeviceOperator.cs
@@ -7,12 +7,16 @@ public class DeviceOperator : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetButtonDown("Fire3")) //реакция на кнопку ввода, заданную в настройках ввода в Unity
+        if (Input.GetButtonDown("Interract")) //реакция на кнопку ввода, заданную в настройках ввода в Unity
         {
             Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius); //метод OverlapSphere() возвращает список ближайших объектов
             foreach (Collider hitCollider in hitColliders)
             {
-                hitCollider.SendMessage("Operate", SendMessageOptions.DontRequireReceiver); //метод SendMessage() пытается вызвать именованную функцию независимо от типа целевого объекта
+                Vector3 direction = hitCollider.transform.position - transform.position;
+                if (Vector3.Dot(transform.forward, direction) > .5f) //отправляем сообщение только при корректной ориентации персонажа
+                {
+                    hitCollider.SendMessage("Operate", SendMessageOptions.DontRequireReceiver); //метод SendMessage() пытается вызвать именованную функцию независимо от типа целевого объекта
+                }
             }
         }
     }
