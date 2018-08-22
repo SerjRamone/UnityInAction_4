068a5a18de1d67ba3c68aa7d7a78c9633e7ef82c
SerjRamone <sergey.greznov@gmail.com>
2018-08-22 23:59:43 +0300

    открывание двери

---
+++ Assets/Scripts/DeviceOperator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DeviceOperator : MonoBehaviour
+{
+    public float radius = 1.5f; //расстояние с которого персонаж может активировать устройство
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire3")) //реакция на кнопку ввода, заданную в настройках ввода в Unity
+        {
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius); //метод OverlapSphere() возвращает список ближайших объектов
+            foreach (Collider hitCollider in hitColliders)
+            {
+                hitCollider.SendMessage("Operate", SendMessageOptions.DontRequireReceiver); //метод SendMessage() пытается вызвать именованную функцию независимо от типа целевого объекта
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/DoorOpenDevice.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DoorOpenDevice : MonoBehaviour
+{
+    [SerializeField] private Vector3 dPos; // смещение, применяемое при открывании двери
+
+    private bool _open; // Переменная для слежения за открытым состоянием двери
+
+    public void Operate()
+    {
+        if (_open) //открываем или закрываем дверь в зависимости от её состояния
+        {
+            Vector3 pos = transform.position - dPos;
+            transform.position = pos;
+        }
+        else
+        {
+            Vector3 pos = transform.position + dPos;
+            transform.position = pos;
+        }
+
+        _open = !_open;
+    }
+}
