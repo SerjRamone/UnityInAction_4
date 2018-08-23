04261bf34fdd26a3af4b1d15d12510b955f717a7
SerjRamone <sergey.greznov@gmail.com>
2018-08-23 22:20:41 +0300

    монитор

---
+++ Assets/Scripts/ColorChangeDevice.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ColorChangeDevice : MonoBehaviour
+{
+    public void Operate()
+    {
+        Color random = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)); //RGB значение в диапазоне от 0 до 1
+        GetComponent<Renderer>().material.color = random; //задаем цвет материала
+    }
+}
